f3d3004472c3db77160ec99c10080dfe6d0a61a6
Ayush Bhople <77747115+abhople1902@users.noreply.github.com>
2024-05-06 14:20:17 +0530

    Initial Commit

---
+++ StudentCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudentCoreData
+//
+//  Created by Ayush Bhople on 06/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
