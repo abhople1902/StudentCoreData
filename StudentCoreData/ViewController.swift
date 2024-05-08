89e18fc8fc26eabb03417d3e4d166e649b8a9ae2
Ayush Bhople <77747115+abhople1902@users.noreply.github.com>
2024-05-08 12:44:10 +0530

    Pilot

--- StudentCoreData/ViewController.swift
+++ StudentCoreData/ViewController.swift
@@ -6,14 +6,82 @@
 //
 
 import UIKit
+import CoreData
+
 
 class ViewController: UIViewController {
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+//    var studentArray = [Student]()
+    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Students.plist")
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print(dataFilePath!)
+        
     }
 
-
+    @IBAction func ITbuttonPressed(_ sender: UIButton) {
+        let request: NSFetchRequest<Student> = Student.fetchRequest()
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        // Do any additional setup after loading the view.
+     
+        do {
+            let students = try context.fetch(request)
+            if !students.isEmpty {
+                for student in students {
+                    if (student.branch! == "IT") {
+                        print("Student Name: \(student.name!)")
+                        print("Student Age: \(student.branch!)")
+                        print("Student Roll: \(student.roll)")
+                    }
+                }
+            } else {
+                print("No students found.")
+            }
+        } catch {
+            print("Error fetching students: \(error)")
+        }
+    }
+    
+    @IBAction func CSEbuttonPressed(_ sender: UIButton) {
+//        let request = NSFetchRequest<NSManagedObject>(entityName: "Student")
+        let request: NSFetchRequest<Student> = Student.fetchRequest()
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        // Do any additional setup after loading the view.
+     
+        do {
+            let students = try context.fetch(request)
+            if !students.isEmpty {
+                for student in students {
+                    if (student.branch! == "CSE") {
+                        print("Student Name: \(student.name!)")
+                        print("Student Age: \(student.branch!)")
+                        print("Student Roll: \(student.roll)")
+                    }
+                }
+            } else {
+                print("No students found.")
+            }
+        } catch {
+            print("Error fetching students: \(error)")
+        }
+    }
+    
+    
+    @IBAction func addData(_ sender: UIButton) {
+        let newStudent = Student(context: context)
+        newStudent.name = "Ayush Bhople"
+        newStudent.roll = 28
+        newStudent.branch = "CSE" // Change branch as needed
+        do {
+            try context.save()
+        } catch {
+            print("Error : - \(error)")
+        }
+    }
+    
 }
 
